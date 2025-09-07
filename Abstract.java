aa703af9b2123674d1b8ca25606768afa9f8febc
vardhan2824 <p.v.vardhan2004@gmail.com>
2025-09-07 19:45:55 +0530

    Abstract file

---
+++ Abstract.java
@@ -0,0 +1,29 @@
+// Code from filename: Main.java
+// abstract class
+abstract class Second {
+  public String fname = "John";
+  public int age = 24;
+  public abstract void study(); // abstract method
+}
+
+// Subclass (inherit from Main)
+class Student extends Second {
+  public int graduationYear = 2018;
+  public void study() { // the body of the abstract method is provided here
+    System.out.println("Studying all day long");
+  }
+}
+// End code from filename: Main.java
+
+// Code from filename: Second.java
+class Abstract {
+  public static void main(String args[]) {
+    // create an object of the Student class (which inherits attributes and methods from Main)
+    Student myObj = new Student();
+
+    System.out.println("Name: " + myObj.fname);
+    System.out.println("Age: " + myObj.age);
+    System.out.println("Graduation Year: " + myObj.graduationYear);
+    myObj.study(); // call abstract method
+  }
+}
