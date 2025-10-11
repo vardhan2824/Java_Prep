61c8bf222eb032d5a1b235075522991431031181
vardhan2824 <p.v.vardhan2004@gmail.com>
2025-10-11 22:40:39 +0530

    Going through basics

--- Test1.java
+++ Test1.java
@@ -1,3 +1,21 @@
-class Test1{
-	
+import static java.lang.System.*;
+import static java.lang.Runtime.*;
+
+class Test1
+{
+	Test1(){
+		out.println("Constructor");
+	}
+	Test1(int i){
+		out.println("parameterised constructor");//Constructor Overloading
+	}
+	public static void main(String[] args){
+		int i = 0;
+		Test1 t = new Test1(i);
+		out.println("static import!");
+		out.println(t.hashCode());
+		gc();
+		getRuntime().gc();//Runtime
+		out.println("Garbage collected");
+	}
 }
