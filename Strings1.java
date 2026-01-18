dc589b42d021b039efe0a68437cc2ecc4e884b72
vardhan2824 <p.v.vardhan2004@gmail.com>
2026-01-18 20:10:01 +0530

    Strings.java

---
+++ Strings1.java
@@ -0,0 +1,20 @@
+import static java.lang.System.*;
+public class Strings1 {
+	public static void main(String[] args){
+		// String s;
+		// s="ABCED";
+		
+		// String s="ABCED";
+		
+		// String s = new String("ABCED");
+		// out.println(s);
+		
+		char arr[]={'A','B','C','D','E'};
+		// String chararray=new String(arr);
+		// System.out.println(chararray);
+		
+		String s=new String(arr,1,3);
+		out.println(s);
+		
+	}
+}
